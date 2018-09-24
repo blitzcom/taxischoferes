28e8bf20b5b2f3b3b375ee677baff3de8949ba27
Diego Lopez <aud0l@hotmail.com>
2018-09-24 05:31:41 -0500

    Cambios de estilo en splash

--- screens/Splash.js
+++ screens/Splash.js
@@ -6,7 +6,8 @@
 import React, { Component } from "react";
 import firebase from "react-native-firebase";
 import type { NavigationScreenProp } from "react-navigation";
-import { AsyncStorage, View, Text } from "react-native";
+import { AsyncStorage } from "react-native";
+import { View, Text, Title, Spinner, Caption } from "@shoutem/ui";
 
 type Props = { navigation: NavigationScreenProp<*> };
 
@@ -23,7 +24,6 @@ export default class Splash extends Component<Props> {
 
   async componentDidMount() {
     const link = await firebase.links().getInitialLink();
-
     if (link !== null && firebase.auth().isSignInWithEmailLink(link)) {
       try {
         const email = await AsyncStorage.getItem("emailForSignIn");
@@ -33,13 +33,11 @@ export default class Splash extends Component<Props> {
         console.warn(error);
       }
     }
-
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.props.navigation.replace("Home");
         return;
       }
-
       this.props.navigation.replace("SignIn");
     });
   }
@@ -47,7 +45,17 @@ export default class Splash extends Component<Props> {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Splash Screen</Text>
+        <View style={{ width: "80%" }}>
+          <Title styleName="h-center" style={{ marginBottom: 2 }}>
+            ¡HEY! TAXI
+          </Title>
+
+          <Caption styleName="h-center" style={{ marginBottom: 40 }}>
+            Conductores
+          </Caption>
+
+          <Spinner />
+        </View>
       </View>
     );
   }
