20e77a7e7d3392c72e040f0990087f9dee7fc501
Diego Lopez <aud0l@hotmail.com>
2018-09-25 00:01:24 -0500

    Agregar botón para disponibilidad de conductor

--- screens/Home.js
+++ screens/Home.js
@@ -6,16 +6,30 @@
 import React, { Component } from "react";
 import firebase from "react-native-firebase";
 import type { NavigationScreenProps } from "react-navigation";
-import { View, Text, Button } from "react-native";
+import { View } from "react-native";
+import { Button, Text, Switch } from "@shoutem/ui";
+import MapView from "react-native-maps";
 
 type Props = {
   navigation: NavigationScreenProps<*>
 };
 
-export default class Home extends Component<Props> {
+type State = {
+  available: false
+};
+
+export default class Home extends Component<Props, State> {
   static navigationOptions = {
     header: null
   };
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      available: false
+    };
+  }
 
   onSignOut = async () => {
     try {
@@ -28,9 +42,36 @@ export default class Home extends Component<Props> {
 
   render() {
     return (
-      <View>
-        <Text>Home</Text>
-        <Button onPress={this.onSignOut} title="Cerrar sesión" />
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        <View
+          style={{
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingBottom: 8,
+            paddingLeft: 12,
+            paddingRight: 12,
+            paddingTop: 8
+          }}
+        >
+          <Text styleName="bold">Disponible</Text>
+          <Switch
+            value={this.state.available}
+            onValueChange={available => this.setState({ available })}
+          />
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <MapView
+            style={{ height: "100%" }}
+            region={{
+              latitude: 37.78825,
+              latitudeDelta: 0.015,
+              longitude: -122.4324,
+              longitudeDelta: 0.0121
+            }}
+          />
+        </View>
       </View>
     );
   }
