ba64ad7a2624d4aa986d09e3e843438e0f895e64
Diego Lopez <aud0l@hotmail.com>
2018-09-26 18:21:50 -0500

    Extrar compoente Map

---
+++ components/Map.js
@@ -0,0 +1,91 @@
+/**
+ * @format
+ * @flow
+ */
+
+import React, { Component, Fragment } from "react";
+import {
+  View,
+  Button,
+  Text,
+  Switch,
+  Title,
+  Spinner,
+  ImageBackground
+} from "@shoutem/ui";
+import MapView, { Marker } from "react-native-maps";
+
+export default class Map extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      available: false,
+      latitude: null,
+      longitude: null
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position =>
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }),
+      error => console.warn(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000
+      }
+    );
+  }
+
+  render() {
+    const { latitude, longitude } = this.state;
+    const hasPosition = latitude !== null && longitude !== null;
+
+    return (
+      <Fragment>
+        <View
+          style={{
+            alignItems: "center",
+            borderBottomWidth: 1,
+            borderColor: "#eaeaea",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingBottom: 8,
+            paddingLeft: 12,
+            paddingRight: 12,
+            paddingTop: 8
+          }}
+        >
+          <Text styleName="bold">Disponible</Text>
+          <Switch
+            value={this.state.available}
+            onValueChange={available => this.setState({ available })}
+          />
+        </View>
+
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          {hasPosition ? (
+            <MapView
+              style={{ height: "100%" }}
+              region={{
+                latitude: latitude,
+                latitudeDelta: 0.003,
+                longitude: longitude,
+                longitudeDelta: 0.003
+              }}
+            >
+              <Marker coordinate={{ latitude, longitude }} />
+            </MapView>
+          ) : (
+            <Spinner />
+          )}
+        </View>
+      </Fragment>
+    );
+  }
+}
--- screens/Home.js
+++ screens/Home.js
@@ -5,30 +5,11 @@
 
 import React, { Component } from "react";
 import firebase from "react-native-firebase";
-import type { NavigationScreenProps } from "react-navigation";
-import {
-  View,
-  Button,
-  Text,
-  Switch,
-  Title,
-  Spinner,
-  ImageBackground
-} from "@shoutem/ui";
-import MapView, { Marker } from "react-native-maps";
+import { View, Button, Text, Title, ImageBackground } from "@shoutem/ui";
 
-type Props = {
-  navigation: NavigationScreenProps<*>
-};
+import Map from "../components/Map";
 
-type State = {
-  available: boolean,
-  latitude: number,
-  longitude: number,
-  passenger: boolean
-};
-
-export default class Home extends Component<Props, State> {
+export default class Home extends Component {
   static navigationOptions = {
     header: null
   };
@@ -37,27 +18,8 @@ export default class Home extends Component<Props, State> {
     super(props);
 
     this.state = {
-      available: false,
       passenger: false,
-      latitude: null,
-      longitude: null
     };
-  }
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      position =>
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        }),
-      error => console.warn(error),
-      {
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 1000
-      }
-    );
   }
 
   onSignOut = async () => {
@@ -70,56 +32,19 @@ export default class Home extends Component<Props, State> {
   };
 
   render() {
-    const { latitude, longitude } = this.state;
-    const hasPosition = latitude !== null && longitude !== null;
+    const { passenger } = this.state;
 
     return (
       <View style={{ flex: 1, backgroundColor: "white", position: "relative" }}>
-        <View
-          style={{
-            alignItems: "center",
-            borderBottomWidth: 1,
-            borderColor: "#eaeaea",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingBottom: 8,
-            paddingLeft: 12,
-            paddingRight: 12,
-            paddingTop: 8
-          }}
-        >
-          <Text styleName="bold">Disponible</Text>
-          <Switch
-            value={this.state.available}
-            onValueChange={available => this.setState({ available })}
-          />
-        </View>
+        <Map />
 
-        <View style={{ flex: 1, justifyContent: "center" }}>
-          {hasPosition ? (
-            <MapView
-              style={{ height: "100%" }}
-              region={{
-                latitude: latitude,
-                latitudeDelta: 0.003,
-                longitude: longitude,
-                longitudeDelta: 0.003
-              }}
-            >
-              <Marker coordinate={{ latitude, longitude }} />
-            </MapView>
-          ) : (
-            <Spinner />
-          )}
-        </View>
-
-        {this.state.available && (
+        {passenger && (
           <View
             style={{
               alignItems: "center",
               height: "100%",
               position: "absolute",
-              width: "100%",
+              width: "100%"
             }}
           >
             <ImageBackground
