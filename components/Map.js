c121fbae99216e53afd2c1c4fe8b0b3bb3b84712
Diego Lopez <aud0l@hotmail.com>
2018-09-26 19:39:20 -0500

    Escuchar nuevos viajes

--- components/Map.js
+++ components/Map.js
@@ -6,19 +6,35 @@
 import React, { Component, Fragment } from "react";
 import { View, Text, Switch, Spinner } from "@shoutem/ui";
 import MapView, { Marker } from "react-native-maps";
+import firebase from "react-native-firebase";
 
-export default class Map extends Component {
-  constructor(props) {
+type Props = {
+  onNewTrip: (data: any) => void
+};
+
+export default class Map extends Component<Props> {
+  constructor(props: Props) {
     super(props);
 
     this.state = {
-      available: false,
+      isAvailable: false,
+      isAvailabilityChanging: false,
       latitude: null,
       longitude: null
     };
+
+    this.onAvailableChange = this.onAvailableChange.bind(this);
+    this.onNewTrip = this.onNewTrip.bind(this);
   }
 
   componentDidMount() {
+    this.tripsRef = firebase
+      .database()
+      .ref(`tripsByDrivers/${firebase.auth().currentUser.uid}`)
+      .orderByChild("state")
+      .equalTo("pending")
+      .limitToFirst(1);
+
     navigator.geolocation.getCurrentPosition(
       position =>
         this.setState({
@@ -34,8 +50,49 @@ export default class Map extends Component {
     );
   }
 
+  componentWillUnmount() {
+    if (this.tripsRef) {
+      this.tripsRef.off("child_added", this.onNewTrip);
+    }
+  }
+
+  syncSetState = nextState => {
+    return new Promise(resolve => {
+      this.setState(nextState, resolve);
+    });
+  };
+
+  onNewTrip(data) {
+    this.props.onNewTrip(data.val());
+  }
+
+  makeAvailable() {
+    this.tripsRef.on("child_added", this.onNewTrip);
+  }
+
+  makeUnavailable() {
+    this.tripsRef.off("child_added", this.onNewTrip);
+  }
+
+  async onAvailableChange(isAvailable) {
+    await this.syncSetState({ isAvailable, isAvailabilityChanging: true });
+
+    if (isAvailable) {
+      this.makeAvailable();
+    } else {
+      this.makeUnavailable();
+    }
+
+    await this.syncSetState({ isAvailabilityChanging: false });
+  }
+
   render() {
-    const { latitude, longitude } = this.state;
+    const {
+      latitude,
+      longitude,
+      isAvailable,
+      isAvailabilityChanging
+    } = this.state;
     const hasPosition = latitude !== null && longitude !== null;
 
     return (
@@ -54,10 +111,17 @@ export default class Map extends Component {
           }}
         >
           <Text styleName="bold">Disponible</Text>
-          <Switch
-            value={this.state.available}
-            onValueChange={available => this.setState({ available })}
-          />
+
+          <View styleName="horizontal v-center">
+            {isAvailabilityChanging && <Spinner style={{ marginRight: 8 }} />}
+
+            <Switch
+              value={isAvailable}
+              onValueChange={
+                isAvailabilityChanging ? () => {} : this.onAvailableChange
+              }
+            />
+          </View>
         </View>
 
         <View style={{ flex: 1, justifyContent: "center" }}>
@@ -81,3 +145,7 @@ export default class Map extends Component {
     );
   }
 }
+
+Map.defaultProps = {
+  onNewTrip: () => {}
+};
