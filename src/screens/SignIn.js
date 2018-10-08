1baf2b8da19308bbf503a5031348667aa4b96192
Diego Lopez <aud0l@hotmail.com>
2018-10-07 22:10:27 -0500

    Corregir error que no permite guardar y redireccionar al iniciar sesión

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -2,18 +2,11 @@
  * @format
  * @flow
  */
-
-import { AsyncStorage } from "react-native";
 import firebase from "react-native-firebase";
 import React, { Component, Fragment } from "react";
-import {
-  View,
-  Caption,
-  Text,
-  Title,
-  FormGroup
-} from "@shoutem/ui";
-import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+import { View, Caption, Text, Title, FormGroup } from "@shoutem/ui";
+import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin";
+GoogleSignin.configure();
 
 type Props = {
   navigation: any
@@ -34,8 +27,6 @@ export default class SignIn extends Component<Props, State> {
     this.state = {
       isSending: false
     };
-
-    this.onAuthStateChanged = this.onAuthStateChanged.bind(this);
   }
 
   asyncState = (state: any) => {
@@ -44,54 +35,48 @@ export default class SignIn extends Component<Props, State> {
     });
   };
 
-  googleLogin = async () => {
+  signIn = async () => {
     try {
       await this.asyncState({ isSending: true });
-      await GoogleSignin.configure();
       const data = await GoogleSignin.signIn();
 
-      const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken)
+      const credential = firebase.auth.GoogleAuthProvider.credential(
+        data.idToken,
+        data.accessToken
+      );
 
-      await firebase.auth().signInWithCredential(credential);
+      const { user } = await firebase.auth().signInWithCredential(credential);
 
-      await AsyncStorage.setItem("userData", data);
-      await this.asyncState({ isSending: false });
-      
-
+      await this.saveUserAndRedirect(user);
     } catch (error) {
       console.warn(error);
+      this.props.navigation.replace("SignIn");
+    }
+  };
+
+  saveUserAndRedirect = async user => {
+    const userRef = firebase.database().ref(`drivers/${user.uid}`);
+    const docsRef = firebase.database().ref(`docs/${user.uid}`);
+
+    await userRef.set({
+      displayName: user.displayName,
+      email: user.email,
+      emailVerified: user.emailVerified,
+      isAnonymous: false,
+      photoURL: user.photoURL,
+      providerId: user.providerId,
+      uid: user.uid
+    });
+
+    const docsSnap = await docsRef.once("value");
+    const docs = docsSnap.val();
+
+    if (docs === null) {
+      return this.props.navigation.replace("Taxi");
     }
 
-    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
-  }
-
-  async onAuthStateChanged(user) {
-    if (user) {
-      const userRef = firebase.database().ref(`drivers/${user.uid}`);
-      const docsRef = firebase.database().ref(`docs/${user.uid}`);
-
-      await userRef.set({
-        displayName: user.displayName,
-        email: user.email,
-        emailVerified: user.emailVerified,
-        isAnonymous: false,
-        photoURL: user.photoURL,
-        providerId: user.providerId,
-        uid: user.uid
-      });
-
-      const docsSnap = await docsRef.once("value");
-      const docs = docsSnap.val();
-
-      if (docs === null) {
-        return this.props.navigation.replace("Taxi");
-      }
-
-      return this.props.navigation.replace("Home");
-    }
-
-    this.props.navigation.replace("SignIn");
-  }
+    return this.props.navigation.replace("Home");
+  };
 
   render() {
     const { isSending } = this.state;
@@ -108,22 +93,20 @@ export default class SignIn extends Component<Props, State> {
           </Caption>
 
           <Fragment>
-              <Text styleName="h-center" style={{ marginBottom: 18 }}>
-                Accede con tu cuenta de google.
-              </Text>
+            <Text styleName="h-center" style={{ marginBottom: 18 }}>
+              Accede con tu cuenta de google.
+            </Text>
 
-              <FormGroup>
-                <GoogleSigninButton
-                  style={{ width: 280, height: 48 }}
-                  size={GoogleSigninButton.Size.Standard}
-                  color={GoogleSigninButton.Color.Dark}
-                  onPress={this.googleLogin}
-                  disabled={isSending}
-                />
-              </FormGroup>
-
-            </Fragment>
-          
+            <FormGroup>
+              <GoogleSigninButton
+                style={{ width: 280, height: 48 }}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Light}
+                onPress={this.signIn}
+                disabled={isSending}
+              />
+            </FormGroup>
+          </Fragment>
         </View>
       </View>
     );
