466b7bc2f857c67cb2d0e2f9ff58f3bdf88485f5
Diego Lopez <aud0l@hotmail.com>
2018-10-10 08:08:55 -0500

    Iniciar TripListener si el conductor está como disponible

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,7 @@
+// @flow
 import React, { Component } from 'react';
 import firebase from 'react-native-firebase';
+import { StyleSheet } from 'react-native';
 import { View } from '@shoutem/ui';
 
 import Map from '../components/Map';
@@ -11,7 +13,15 @@ type Props = {
   navigation: any,
 };
 
-export default class Home extends Component<Props> {
+type State = {
+  destiny: null | Object,
+  isAvailable: boolean,
+  origin: null | Object,
+  tripId: null | string,
+  uid: string,
+};
+
+export default class Home extends Component<Props, State> {
   static navigationOptions = {
     header: null,
   };
@@ -19,14 +29,19 @@ export default class Home extends Component<Props> {
   constructor(props: Props) {
     super(props);
 
-    this.state = {
-      tripId: null,
-      origin: null,
-      destiny: null,
-      uid: firebase.auth().currentUser.uid,
-    };
+    const user = firebase.auth().currentUser;
 
-    this.onNewTrip = this.onNewTrip.bind(this);
+    if (user === null) {
+      throw new Error('CurrentUser is null in Home.');
+    }
+
+    this.state = {
+      destiny: null,
+      isAvailable: false,
+      origin: null,
+      tripId: null,
+      uid: user.uid,
+    };
   }
 
   onSignOut = async () => {
@@ -38,21 +53,28 @@ export default class Home extends Component<Props> {
     }
   };
 
-  onNewTrip(trip) {
+  onNewTrip = (trip: string) => {
     this.setState({ tripId: trip });
-  }
+  };
 
   onDismiss = () => {
     this.setState({ tripId: null });
   };
 
+  onChangeAvailability = (isAvailable: boolean) => {
+    this.setState({ isAvailable });
+  };
+
   render() {
-    const { tripId, origin, destiny, uid } = this.state;
+    const { tripId, origin, destiny, uid, isAvailable } = this.state;
 
     return (
       <View style={{ flex: 1, backgroundColor: 'white', position: 'relative' }}>
-        <TripListener onNewTrip={this.onNewTrip} />
-        <LocationTracker userId={uid} />
+        <TripListener
+          onNewTrip={this.onNewTrip}
+          onChangeAvailability={this.onChangeAvailability}
+        />
+        {isAvailable && <LocationTracker userId={uid} />}
         <Map onNewTrip={this.onNewTrip} origin={origin} destiny={destiny} />
         {tripId && (
           <Machine
